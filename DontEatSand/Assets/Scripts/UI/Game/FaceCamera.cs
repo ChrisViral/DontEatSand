bee5ec334ee3b27a4156d0229ece836598fbf9d2
Christophe Savard <christophe_savard@hotmail.ca>
2020-04-16 07:17:54 -0400

    Update FaceCamera

---
+++ DontEatSand/Assets/Scripts/UI/Game/FaceCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace DontEatSand.UI.Game
+{
+    [RequireComponent(typeof(Canvas))]
+    public class FaceCamera : MonoBehaviour
+    {
+        #region Fields
+        private Canvas canvas;
+        private new Transform camera;
+        #endregion
+
+        #region Functions
+        private void Awake()
+        {
+            Camera cam = Camera.main;
+            this.camera = cam.transform;
+            GetComponent<Canvas>().worldCamera = cam;
+        }
+
+        private void Update() => this.transform.LookAt(this.camera, this.camera.up);
+        #endregion
+    }
+}
+
