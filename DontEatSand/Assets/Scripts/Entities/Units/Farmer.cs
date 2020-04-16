e328b2dbb629e6bb4f176166331570d61b368ebc
Natalia Whiteley <nataliawhiteley11@gmail.com>
2020-04-16 14:50:54 -0400

    Added some farmer behavior.

--- DontEatSand/Assets/Scripts/Entities/Units/Farmer.cs
+++ DontEatSand/Assets/Scripts/Entities/Units/Farmer.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using DontEatSand.Base;
+using UnityEngine;
+using UnityEngine.AI;
 
 namespace DontEatSand.Entities.Units
 {
@@ -12,6 +14,51 @@ namespace DontEatSand.Entities.Units
         /// </summary>
         private int buildTriggerName = Animator.StringToHash("Building");
         #endregion
+
+
+        #region Methods
+
+        protected override void ProcessCommand(Vector3 destination, ISelectable target)
+        {
+            base.ProcessCommand(destination, target);
+
+            // If target is wet sand, dig
+            if (target is Discoverable discoverable)
+            {
+                this.HasOrderFlag = true;
+                this.Destination = discoverable.transform.position;
+                Dig();
+            }
+            // Else if target is building, build
+            else if (target is Entity entity)
+            {
+                this.HasOrderFlag = true;
+                this.Target = entity;
+                Build();
+            }
+        }
         
+        public void Dig()
+        {
+            if (this.agent.pathStatus == NavMeshPathStatus.PathComplete)
+            {
+                // Set animation trigger
+                animator.SetTrigger(digTriggerName);
+                
+                // Take sand from sandpit
+            }
+        }
+
+        public void Build()
+        {
+            if (this.agent.pathStatus == NavMeshPathStatus.PathComplete)
+            {
+                // Set animation trigger
+                animator.SetTrigger(buildTriggerName);
+                
+                // Build building
+            }
+        }
+        #endregion
     }
 }
