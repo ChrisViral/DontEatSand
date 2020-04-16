03e3b9a822701853bcb67c8807bcf8995b2c5534
Natalia Whiteley <nataliawhiteley11@gmail.com>
2020-04-16 13:17:13 -0400

    Added farmer and range animation triggers.

--- DontEatSand/Assets/Scripts/Entities/Units/Farmer.cs
+++ DontEatSand/Assets/Scripts/Entities/Units/Farmer.cs
@@ -1,6 +1,17 @@
-﻿namespace DontEatSand.Entities.Units
+﻿using UnityEngine;
+
+namespace DontEatSand.Entities.Units
 {
     public class Farmer : Unit
     {
+        #region Constants
+        /// Animator dig trigger name
+        /// </summary>
+        private int digTriggerName = Animator.StringToHash("Digging");
+        /// Animator buil trigger name
+        /// </summary>
+        private int buildTriggerName = Animator.StringToHash("Building");
+        #endregion
+        
     }
 }
