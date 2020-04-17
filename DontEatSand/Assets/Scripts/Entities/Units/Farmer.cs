f7830a9669ad4902539bc861c636de8f4044ff1b
Natalia Whiteley <nataliawhiteley11@gmail.com>
2020-04-16 21:16:17 -0400

    Started farmer behavior tree.

--- DontEatSand/Assets/Scripts/Entities/Units/Farmer.cs
+++ DontEatSand/Assets/Scripts/Entities/Units/Farmer.cs
@@ -1,21 +1,58 @@
 ﻿using DontEatSand.Base;
 using DontEatSand.Entities.Buildings;
+using DontEatSand.Utils.BehaviourTrees;
 using UnityEngine;
 using UnityEngine.AI;
+using BTCoroutine = System.Collections.Generic.IEnumerator<DontEatSand.Utils.BehaviourTrees.BTNodeResult>;
 
 namespace DontEatSand.Entities.Units
 {
+    /// <summary>
+    /// Unit AI mode
+    /// </summary>
+    public enum FarmerMode
+    {
+        DIG,
+        BUILD,
+        FLEE
+    }
+    
     public class Farmer : Unit
     {
         #region Constants
         /// Animator dig trigger name
         /// </summary>
-        private int digTriggerName = Animator.StringToHash("Digging");
+        private static readonly int digParam = Animator.StringToHash("Digging");
         /// Animator build trigger name
         /// </summary>
-        private int buildTriggerName = Animator.StringToHash("Building");
+        private static readonly int buildParam = Animator.StringToHash("Building");
         #endregion
 
+        #region Fields
+        public FarmerMode behaviourMode;
+        private float digStart;
+        private float digInterval = 3.0f;
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// If the unit can currently attack
+        /// </summary>
+        private bool CanDig {
+            get
+            {
+                bool digReady = false;
+                if(Time.time > this.digStart + this.digInterval)
+                {
+                    this.digStart = Time.time;
+                    digReady = true;
+                }
+                return digReady && this.Target != null && Vector3.Distance(this.Position, this.Target.Position) < 1.0f;
+            }
+        }
+
+        #endregion
 
         #region Methods
 
@@ -41,10 +78,10 @@ namespace DontEatSand.Entities.Units
         
         public void Dig(Sandpit sandpit)
         {
-            if (this.agent.pathStatus == NavMeshPathStatus.PathComplete)
+            if (CanDig)
             {
                 // Set animation bool for digging
-                animator.SetBool(digTriggerName, true);
+                animator.SetBool(digParam, true);
                 
                 // Take sand from sandpit
                 sandpit.HarvestSand(10);
@@ -56,11 +93,54 @@ namespace DontEatSand.Entities.Units
             if (this.agent.pathStatus == NavMeshPathStatus.PathComplete)
             {
                 // Set animation boll for building
-                animator.SetBool(buildTriggerName, true);
+                animator.SetBool(buildParam, true);
                 
                 // Build building
             }
         }
+        #endregion
+
+        #region BehaviourTree
+
+        /// <summary>
+        /// Routine for the dig leaf
+        /// </summary>
+        /// <returns></returns>
+        [BTLeaf("dig")]
+        public BTCoroutine DigRoutine()
+        {
+            if (this.behaviourMode == FarmerMode.DIG)
+            {
+
+            }
+        }
+        
+        /// <summary>
+        /// Routine for the build leaf
+        /// </summary>
+        /// <returns></returns>
+        [BTLeaf("build")]
+        public BTCoroutine BuildRoutine()
+        {
+            if (this.behaviourMode == FarmerMode.BUILD)
+            {
+
+            }
+        }
+        
+        /// <summary>
+        /// Routine for the flee leaf
+        /// </summary>
+        /// <returns></returns>
+        [BTLeaf("flee")]
+        public BTCoroutine FleeRoutine()
+        {
+            if (this.behaviourMode == FarmerMode.FLEE)
+            {
+
+            }
+        }
+
         #endregion
     }
 }
