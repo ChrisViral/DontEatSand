f192bc7c3267dc5f58678e698b2619af1000e6d0
Natalia Whiteley <nataliawhiteley11@gmail.com>
2020-04-17 16:10:43 -0400

    Added scout behavior tree and fleeing.

--- DontEatSand/Assets/Scripts/Entities/Units/Snitch.cs
+++ DontEatSand/Assets/Scripts/Entities/Units/Snitch.cs
@@ -1,6 +1,41 @@
-﻿namespace DontEatSand.Entities.Units
+﻿using UnityEngine;
+using System.Collections.Generic;
+using DontEatSand.Utils.BehaviourTrees;
+using BTCoroutine = System.Collections.Generic.IEnumerator<DontEatSand.Utils.BehaviourTrees.BTNodeResult>;
+
+
+namespace DontEatSand.Entities.Units
 {
     public class Snitch : Unit
     {
+
+        #region Methods
+
+        public void Flee()
+        {
+            // Set destination away from enemy
+            Destination = transform.position - FindClosestTarget().transform.position;
+        }
+
+        #endregion
+
+        #region BehaviourTree
+        
+        /// <summary>
+        /// Routine for the flee leaf
+        /// </summary>
+        /// <returns></returns>
+        [BTLeaf("flee")]
+        public BTCoroutine FleeRoutine()
+        {
+            if (IsUnderAttackFlag)
+            {
+                Flee();
+                yield return BTNodeResult.NOT_FINISHED;
+            }
+            yield return BTNodeResult.SUCCESS;
+        }
+
+        #endregion
     }
 }
